b351ae129a9eb95d38bb1480ca81ae601eaaabe5
Manuel Reber <reberma@student.ethz.ch>
2021-04-28 11:57:02 +0200

    added data preprocessor and an example

---
+++ examples/fetch_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from surprise import SVD, Dataset, Reader
+from surprise.model_selection import cross_validate
+
+from data_preprocess import Data
+
+df = Data.get_df()
+reader = Reader(rating_scale=(1, 5))
+data = Dataset.load_from_df(df[["userID", "itemID", "rating"]], reader)
+
+algo = SVD()
+
+cross_validate(algo, data, measures=["RMSE"], cv=5, verbose=True)
