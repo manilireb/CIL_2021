0048a71cdb77a460c6292b0ddf23fd016411bf86
Manuel Reber <reberma@student.ethz.ch>
2021-05-13 10:31:55 +0200

    added comments

--- examples/hyperparameters_svd.py
+++ examples/hyperparameters_svd.py
@@ -4,6 +4,11 @@ sys.path.append("../src/")
 
 from MF_SVD import MFSVD
 
+"""
+Please do not run this file on your local machine because it would probably take several hours to complete.
+Just use it as a reference on how one can use the defined classes.
+"""
+
 SVDBiased = MFSVD(biased=True)
 SVDUnBiased = MFSVD(biased=False)
 
