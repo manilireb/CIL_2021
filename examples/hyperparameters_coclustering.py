5d3d4e94dd79eb5f4291b0bb2b45547b31dc0243
Manuel Reber <manilireb@gmail.com>
2021-05-18 00:30:13 +0200

    change structure

---
+++ examples/hyperparameters_coclustering.py
@@ -0,0 +1,9 @@
+from Co_Clustering.Clustering_Coclustering import Clustering_Coclustering
+
+"""
+Please do not run this file on your local machine because it would probably take several hours to complete.
+Just use it as a reference on how one can use the defined classes.
+"""
+
+CoCl = Clustering_Coclustering()
+CoCl.log_hyperparameters_to_json()
---
+++ examples/hyperparameters_slope_one.py
@@ -0,0 +1,9 @@
+from SlopeOne.slope_one import Slope_One
+
+"""
+Please do not run this file on your local machine because it would probably take several hours to complete.
+Just use it as a reference on how one can use the defined classes.
+"""
+
+slopeOne = Slope_One()
+slopeOne.log_hyperparameters_to_json()
