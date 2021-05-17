6faa4edcdb41baa869166852b658432f8d9dbd7d
Manuel Reber <61589304+manilireb@users.noreply.github.com>
2021-05-18 00:31:28 +0200

    Delete src/examples directory

--- src/examples/hyperparameters_svdpp.py
+++
@@ -1,9 +0,0 @@
-from MF_Methods.MF_SVDpp import MFSVDpp
-
-"""
-Please do not run this file on your local machine because it would probably take several hours to complete.
-Just use it as a reference on how one can use the defined classes.
-"""
-
-SVDpp = MFSVDpp()
-SVDpp.log_hyperparameters_to_json()
