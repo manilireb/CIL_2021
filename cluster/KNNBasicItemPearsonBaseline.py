5badcf9018ce35be54d438322b13f6a4eb5df416
stedi22 <dsteffen@student.ethz.ch>
2021-06-20 17:13:15 +0200

    add cluster method for KNNBasicItemPearsonBaseline

---
+++ cluster/KNNBasicItemPearsonBaseline.py
@@ -0,0 +1,4 @@
+from KNN_Methods.KNN_Basics import KNN_Basic
+
+KNNBasic_item_pearson_baseline = KNN_Basic(sim_name="pearson_baseline", user_based=False)
+KNNBasic_item_pearson_baseline.log_hyperparameters_to_json()
