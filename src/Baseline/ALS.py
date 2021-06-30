b6cc9a2acf0766e851ece3833da7068546bd82ed
Manuel Reber <manilireb@gmail.com>
2021-06-30 13:15:01 +0200

    fixed bug

--- src/Baseline/ALS.py
+++ src/Baseline/ALS.py
@@ -1,5 +1,3 @@
-from itertools import groupby
-
 import numpy as np
 import scipy.sparse as sp
 from bayes_opt import BayesianOptimization
@@ -12,135 +10,312 @@ from utilities.data_preprocess import Data, get_git_root
 
 
 class ALS:
+    """
+    Class for the ALS baseline algorithm.
+
+    Members
+    --------
+    data_matrix : sparse.lil.lil_matrix
+        The data matrix.
+    nnz_users :
+        list of nonzero user indices
+    nnz_items :
+        list of nonzero item indices
+    indices : np.ndarray
+        list from 0 to the number of nonzero elements in the data
+    num_users: int
+        number of users
+    num_items: int
+        number of items
+    num_epochs: int
+        number of epochs
+    user_matrix: np.ndarray
+        The user matrix
+    item_matrix: np.ndarray
+        The item matrix
+    tuning_params: dict
+        dictionary with the parameter intervals
+
+    """
+
     def __init__(self):
         D = Data()
         self.data_matrix = D.get_sparse_ratings_matrix()
+        self.nnz_users, self.nnz_items = self.data_matrix.nonzero()
+        self.indices = np.arange(self.nnz_users.shape[0])
+        self.num_users = 10000
+        self.num_items = 1000
+        self.num_epochs = 30
         self.random_state = 42
         np.random.seed(self.random_state)
-        self.user_features = None
-        self.item_features = None
+        self.user_matrix = None
+        self.item_matrix = None
         self.tuning_params = {
-            "num_epochs": [5, 100],
             "num_features": [5, 50],
             "lambda_user": [0.0005, 1.0],
             "lambda_item": [0.0005, 1.0],
         }
 
-    def init_MF(self, train, num_features):
-        num_users, num_items = train.get_shape()
+    def init_matrix(self, train, num_features):
+        """
+        This method is used for initializing user_matrix and item_matrix.
 
-        user_features = np.random.rand(num_users, num_features)
-        item_features = np.random.rand(num_items, num_features)
+        Parameters
+        ----------
+        train : sparse.lil.lil_matrix
+            Train data.
+        num_features : int
+            Number of features.
 
+        Returns
+        -------
+        user_matrix : np.ndarray
+            Initialized user matrix.
+        item_matrix : np.ndarray
+            Initialized item matirx.
+
+        """
+        user_matrix = 5 * np.random.rand(self.num_users, num_features)
+        item_matrix = 5 * np.random.rand(num_features, self.num_items)
         item_nnz = train.getnnz(axis=0)
         item_sum = train.sum(axis=0)
+        item_matrix[0, :] = item_sum / item_nnz
+        return user_matrix, item_matrix
 
-        for ind in range(num_items):
-            item_features[ind, 0] = item_sum[0, ind] / item_nnz[ind]
+    def update_user_matrix(self, data, user_matrix, item_matrix, num_features, lambda_user):
+        """
+        Method for update the user matrix.
 
-        return user_features, item_features
+        Parameters
+        ----------
+        data : sparse.lil.lil_matrix
+            Data matrix.
+        user_matrix : np.ndarray
+            User matrix.
+        item_matrix : np.ndarray
+            Item matrix.
+        num_features : int
+            Number of features.
+        lambda_user : float
+            regularization parameter for the user matrix.
 
-    def group_by(self, data, index):
-        sorted_data = sorted(data, key=lambda x: x[index])
-        groupby_data = groupby(sorted_data, lambda x: x[index])
-        return groupby_data
+        Returns
+        -------
+        user_matrix : np.ndarray
+            Updated user matrix.
 
-    def build_index_groups(self, train):
-        nnz_row, nnz_col = train.nonzero()
-        nnz_train = list(zip(nnz_row, nnz_col))
-        grouped_nnz_train_by_row = self.group_by(nnz_train, index=0)
-        nnz_row_col_indices = [(g, np.array([v[1] for v in value])) for g, value in grouped_nnz_train_by_row]
-
-        grouped_nnz_train_by_col = self.group_by(nnz_train, index=1)
-        nnz_col_row_indices = [(g, np.array([v[0] for v in value])) for g, value in grouped_nnz_train_by_col]
-
-        return nnz_train, nnz_row_col_indices, nnz_col_row_indices
-
-    def update_user_feature(self, train, item_features, lambda_user, nnz_items_per_user, nnz_user_item_indices):
-        num_user = nnz_items_per_user.shape[0]
-        num_features = item_features.shape[1]
+        """
         lambda_I = lambda_user * sp.eye(num_features)
-        updated_user_features = np.zeros((num_user, num_features))
+        for u in range(self.num_users):
+            indices = self.get_observed_items_per_user(data, u)
+            V = item_matrix[:, indices]
+            A = V @ V.T + lambda_I
+            B = V @ data[u, indices].T
+            user_matrix[u, :] = np.linalg.solve(A, B).squeeze()
+        return user_matrix
 
-        for user, item in nnz_user_item_indices:
-            M = item_features[item, :]
-            V = M.T @ train[user, item].T
-            A = M.T @ M + nnz_items_per_user[user] * lambda_I
-            X = np.linalg.solve(A, V)
-            updated_user_features[user, :] = np.copy(X.T)
+    def update_item_matrix(self, data, user_matrix, item_matrix, num_features, lambda_item):
+        """
+        Method for update the item matrix
 
-        return updated_user_features
+        Parameters
+        ----------
+        data : sparse.lil.lil_matrix
+            data matrix.
+        user_matrix : np.ndarray
+            User matrix.
+        item_matrix : np.ndarray
+            Item matrix.
+        num_features : int
+            Number of features.
+        lambda_item : float
+            Regularization parameter for the item matirx.
 
-    def update_item_features(self, train, user_features, lambda_item, nnz_users_per_item, nnz_item_user_indices):
-        num_items = nnz_users_per_item.shape[0]
-        num_features = user_features.shape[1]
+        Returns
+        -------
+        item_matrix : np.ndarray
+            Updated item matrix.
+
+        """
         lambda_I = lambda_item * sp.eye(num_features)
-        updated_item_features = np.zeros((num_items, num_features))
+        for i in range(self.num_items):
+            indices = self.get_observed_users_per_item(data, i)
+            U = user_matrix[indices, :]
+            A = U.T @ U + lambda_I
+            B = U.T @ data[indices, i]
+            item_matrix[:, i] = np.linalg.solve(A, B).squeeze()
+        return item_matrix
 
-        for item, user in nnz_item_user_indices:
-            M = user_features[user, :]
-            V = M.T @ train[user, item]
-            A = M.T @ M + nnz_users_per_item[item] * lambda_I
-            X = np.linalg.solve(A, V)
-            updated_item_features[item, :] = np.copy(X.T)
+    def get_train_test_matrix(self, train_indices, test_indices):
+        """
+        Method for creating a sparse train and test matrix
 
-        return updated_item_features
+        Parameters
+        ----------
+        train_indices : np.ndarray
+            Indices of the train data.
+        test_indices : np.ndarray
+            Indices of the test data.
 
-    def compute_rmse(self, data, nnz):
-        mse = 0.0
-        for row, col in nnz:
-            user_info = self.user_features[row, :]
-            item_info = self.item_features[col, :]
-            mse += (data[row, col] - user_info @ item_info) ** 2
+        Returns
+        -------
+        train : sparse.lil.lil_matrix
+            Sparse train data matrix.
+        test : sparse.lil.lil_matrix
+            Sparse test data matrix.
 
-        return np.sqrt(mse / len(nnz))
+        """
+        train_nnz_items = self.nnz_items[train_indices]
+        train_nnz_users = self.nnz_users[train_indices]
+        train = sp.lil_matrix((self.num_users, self.num_items))
+        train[train_nnz_users, train_nnz_items] = self.data_matrix[train_nnz_users, train_nnz_items]
+        test_nnz_items = self.nnz_items[test_indices]
+        test_nnz_users = self.nnz_users[test_indices]
+        test = sp.lil_matrix((self.num_users, self.num_items))
+        test[test_nnz_users, test_nnz_items] = self.data_matrix[test_nnz_users, test_nnz_items]
+        return train, test
 
-    def fit(self, train, num_epochs, num_features, lambda_user, lambda_item):
+    def get_observed_items_per_user(self, data, user_idx):
+        """
+        This method returns a list of all nonzero indices for a given user in the
+        data matirx
 
-        user_features, item_features = self.init_MF(train, num_features)
+        Parameters
+        ----------
+        data : sparse.lil.lil_matrix
+            Data matrix.
+        user_idx : int
+            ID of the user.
 
-        nnz_items_per_user, nnz_users_per_item = train.getnnz(axis=1), train.getnnz(axis=0)
+        Returns
+        -------
+        np.ndarray
+            List of all nonzero indices for the given user.
 
-        nnz_train, nnz_user_item_indices, nnz_item_user_indices = self.build_index_groups(train)
+        """
+        return data.getrow(user_idx).nonzero()[1]
 
-        for it in tqdm(range(num_epochs)):
+    def get_observed_users_per_item(self, data, item_idx):
+        """
+        This method returns a list of all nonzero indices for a given item in the
+        data matrix
 
-            user_features = self.update_user_feature(
-                train, item_features, lambda_user, nnz_items_per_user, nnz_user_item_indices
-            )
+        Parameters
+        ----------
+        data : sparse.lil.lil_matrix
+            Data matrix.
+        item_idx : int
+            ID of the item.
 
-            item_features = self.update_item_features(
-                train, user_features, lambda_item, nnz_users_per_item, nnz_item_user_indices
-            )
+        Returns
+        -------
+        np.ndarray
+            List of all nonzero indices for a given item.
 
-        self.user_features = user_features
-        self.item_features = item_features
+        """
+        return data.getcol(item_idx).nonzero()[0]
+
+    def fit(self, data, num_features, lambda_user, lambda_item):
+        """
+        Function that fits a user and item matrix
+
+        Parameters
+        ----------
+        data : sparse.lil.lil_matrix
+            Data matrix.
+        num_features : int
+            Number of latent features of the data.
+        lambda_user : float
+            Regularization parameter.
+        lambda_item : float
+            Regularization parameter.
+
+        Returns
+        -------
+        None.
+
+        """
+
+        user_matrix, item_matrix = self.init_matrix(data, num_features)
+        for it in tqdm(range(self.num_epochs)):
+            user_matrix = self.update_user_matrix(data, user_matrix, item_matrix, num_features, lambda_user)
+            item_matrix = self.update_item_matrix(data, user_matrix, item_matrix, num_features, lambda_item)
+        self.user_matrix = user_matrix
+        self.item_matrix = item_matrix
 
     def get_test_rmse(self, test):
+        """
+        Return rmse for the test data
+
+        Parameters
+        ----------
+        test : sparse.lil.lil_matrix
+            Test data.
+
+        Returns
+        -------
+        rmse : float
+            RMSE for the test data.
+
+        """
         nnz_row, nnz_col = test.nonzero()
         nnz_test = list(zip(nnz_row, nnz_col))
-        rmse = self.compute_rmse(test, nnz_test)
+        rmse = 0.0
+        for u, i in nnz_test:
+            user = self.user_matrix[u, :]
+            item = self.item_matrix[:, i]
+            pred = user @ item
+            if pred > 5:
+                pred = 5
+            if pred < 1:
+                pred = 1
+            rmse += (self.data_matrix[u, i] - pred) ** 2
+        rmse = np.sqrt(rmse / len(nnz_test))
         return rmse
 
-    def optimizer_function(self, num_epochs, num_features, lambda_user, lambda_item):
-        cv = 5
+    def optimizer_function(self, num_features, lambda_user, lambda_item):
+        """
+        This function is used for optimize the hyperparameters with the Gaussian process.
+
+        Parameters
+        ----------
+        num_features : int
+            Number of latent features.
+        lambda_user : float
+            Regularization parameter for the user.
+        lambda_item : float
+            Regularization parameter for the item.
+
+        Returns
+        -------
+        float
+            mean rmse of a 3-fold cv.
+
+        """
+        cv = 3
         kf = KFold(n_splits=cv, random_state=self.random_state, shuffle=True)
         test_RMSE_list = []
-
-        for train_index, test_index in kf.split(self.data_matrix):
-            train_ratings, test_ratings = self.data_matrix[train_index], self.data_matrix[test_index]
-            self.fit(train_ratings, int(num_epochs), int(num_features), lambda_user, lambda_item)
-            test_rmse = self.get_test_rmse(test_ratings)
+        for train_indices, test_indices in kf.split(self.indices):
+            train, test = self.get_train_test_matrix(train_indices, test_indices)
+            self.fit(train, int(num_features), lambda_user, lambda_item)
+            test_rmse = self.get_test_rmse(test)
             test_RMSE_list.append(test_rmse)
-
         mean_test_rmse = np.mean(test_RMSE_list)
         return -mean_test_rmse
 
     def log_hyperparams_to_json(self):
+        """
+        This function is used for storing the optimal hyperparameters
+
+        Returns
+        -------
+        None.
+
+        """
         optimizer = BayesianOptimization(
             f=self.optimizer_function, pbounds=self.tuning_params, random_state=self.random_state
         )
         path = get_git_root() + "/logs/ALS.json"
         logger = JSONLogger(path=path)
         optimizer.subscribe(Events.OPTIMIZATION_STEP, logger)
-        optimizer.maximize(init_points=7, n_iter=20)
+        optimizer.maximize(init_points=4, n_iter=10)
