9fb8c2d4c3085aa3656600601acc71e3182eaa5f
Manuel Reber <manilireb@gmail.com>
2021-07-07 17:23:46 +0200

    add new method

---
+++ src/Ensemble/ridge_more_models.py
@@ -0,0 +1,89 @@
+import csv
+
+import numpy as np
+from sklearn.linear_model import RidgeCV
+from surprise import Dataset, Reader
+from surprise.model_selection import train_test_split
+
+from Co_Clustering.Clustering_Coclustering import Clustering_Coclustering
+from KNN_Methods.KNN_WithMeans import KNN_WithMeans
+from KNN_Methods.KNN_WithZScore import KNN_WithZScore
+from MF_Methods.MF_NMF import MFNMF
+from MF_Methods.MF_SVD import MFSVD
+from SlopeOne.slope_one import Slope_One
+from utilities.data_preprocess import Data
+
+if __name__ == "__main__":
+
+    df = Data().get_df()
+    reader = Reader(rating_scale=(1, 5))
+    data = Dataset.load_from_df(df[["userID", "itemID", "rating"]], reader)
+
+    models = [
+        KNN_WithMeans(sim_name="pearson_baseline", user_based=False),
+        KNN_WithZScore(sim_name="pearson_baseline", user_based=False),
+        MFNMF(biased=False),
+        KNN_WithZScore(sim_name="pearson", user_based=False),
+        KNN_WithMeans(sim_name="pearson", user_based=False),
+        MFSVD(biased=True),
+        KNN_WithMeans(sim_name="msd", user_based=False),
+        KNN_WithZScore(sim_name="msd", user_based=False),
+        KNN_WithZScore(sim_name="pearson_baseline", user_based=True),
+        KNN_WithMeans(sim_name="pearson_baseline", user_based=True),
+        KNN_WithMeans(sim_name="cosine", user_based=False),
+        Slope_One(),
+        KNN_WithZScore(sim_name="pearson", user_based=True),
+        KNN_WithZScore(sim_name="cosine", user_based=False),
+        KNN_WithMeans(sim_name="pearson", user_based=True),
+        KNN_WithZScore(sim_name="msd", user_based=True),
+        Clustering_Coclustering(),
+    ]
+
+    # getting prediction of models
+    train, test = train_test_split(data, test_size=0.3, random_state=42)
+    predictions = []
+    for model in models:
+        print("fitting: ", model)
+        m = model.get_opt_model()
+        p = m.fit(train).test(test)
+        test_pred = [pred[3] for pred in p]
+        predictions.append(test_pred)
+    pred_array = np.array(predictions).T
+    ground_truth = np.array([pred[2] for pred in p])
+
+    # using CV to learn optimal alpha for Ridge regression default is leave one out CV
+    alphas = np.logspace(-3, np.log10(10), 1000)
+    regressor = RidgeCV(alphas=alphas, store_cv_values=True, cv=None)  # Default scoring MSE
+    regressor.fit(pred_array, ground_truth)
+
+    # loading and preparing prediction set
+    df = Data("sampleSubmission.csv").get_df()
+    test_data = Dataset.load_from_df(df[["userID", "itemID", "rating"]], reader)
+
+    train = data.build_full_trainset()
+    test = test_data.build_full_trainset().build_testset()
+
+    # creating predictions
+    predictions = []
+    for model in models:
+        print("fitting: ", model)
+        m = model.get_opt_model()
+        p = m.fit(train).test(test)
+        test_pred = [pred[3] for pred in p]
+
+    pred_array_submission = np.array(predictions).T
+    UID = np.array([pred[0] for pred in p])
+    IID = np.array([pred[1] for pred in p])
+
+    y_hat = regressor.predict(pred_array_submission)  # applying ridge regression
+
+    # preparing submission
+    pos = ["Id"]
+    pred = ["Prediction"]
+    for i in range(len(y_hat)):
+        pos.append(f"r{UID[i]}_c{IID[i]}")
+        pred.append(y_hat[i])
+
+    with open("submission_ridge_more_models.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(zip(pos, pred))
