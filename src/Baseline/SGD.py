19f315aa77c0f8c6a12516dd9de940dfa7dc4edb
Manuel Reber <manilireb@gmail.com>
2021-07-15 18:47:51 +0200

    added Baseline Method

--- src/Baseline/SGD.py
+++ src/Baseline/SGD.py
@@ -16,32 +16,9 @@ from tqdm import tqdm
 from utilities.data_preprocess import Data, get_git_root
 
 
-class ALS(BaseEstimator, TransformerMixin):
+class SGD(BaseEstimator, TransformerMixin):
     """
-    Class for the ALS baseline algorithm.
-
-    Members
-    --------
-    data_matrix : sparse.lil.lil_matrix
-        The data matrix.
-    nnz_users :
-        list of nonzero user indices
-    nnz_items :
-        list of nonzero item indices
-    indices : np.ndarray
-        list from 0 to the number of nonzero elements in the data
-    num_users: int
-        number of users
-    num_items: int
-        number of items
-    num_epochs: int
-        number of epochs
-    user_matrix: np.ndarray
-        The user matrix
-    item_matrix: np.ndarray
-        The item matrix
-    tuning_params: dict
-        dictionary with the parameter intervals
+    Class for the SGD baseline algorithm
 
     """
 
@@ -57,14 +34,15 @@ class ALS(BaseEstimator, TransformerMixin):
         self.item_matrix = None
         self.tuning_params = {
             "num_features": [2, 50],
-            "lambda_user": [0.0005, 30.0],
-            "lambda_item": [0.0005, 30.0],
+            "lambda_user": [0.0005, 0.9],
+            "lambda_item": [0.0005, 0.9],
+            "gamma": [0.0005, 0.1],
         }
         np.random.seed(self.random_state)
 
     def init_matrix(self, train, num_features):
         """
-        This method is used for initializing user_matrix and item_matrix.
+        This method is used for initializing user_matrix and item_matrix
 
         Parameters
         ----------
@@ -78,7 +56,7 @@ class ALS(BaseEstimator, TransformerMixin):
         user_matrix : np.ndarray
             Initialized user matrix.
         item_matrix : np.ndarray
-            Initialized item matirx.
+            Initialized item matrix.
 
         """
         user_matrix = 5 * np.random.rand(self.num_users, num_features)
@@ -87,71 +65,6 @@ class ALS(BaseEstimator, TransformerMixin):
         item_sum = train.sum(axis=0)
         item_matrix[0, :] = item_sum / item_nnz
         return user_matrix, item_matrix
-
-    def update_user_matrix(self, data, user_matrix, item_matrix, num_features, lambda_user):
-        """
-        Method for update the user matrix.
-
-
-        Parameters
-        ----------
-        data : sparse.lil.lil_matrix
-            Data matrix.
-        user_matrix : np.ndarray
-            User matrix.
-        item_matrix : np.ndarray
-            Item matrix.
-        num_features : int
-            Number of features.
-        lambda_user : float
-            regularization parameter for the user matrix.
-
-        Returns
-        -------
-        user_matrix : np.ndarray
-            Updated user matrix.
-
-        """
-        lambda_I = lambda_user * sp.eye(num_features)
-        for u in range(self.num_users):
-            indices = self.get_observed_items_per_user(data, u)
-            V = item_matrix[:, indices]
-            A = V @ V.T + lambda_I
-            B = V @ data[u, indices].T
-            user_matrix[u, :] = np.linalg.solve(A, B).squeeze()
-        return user_matrix
-
-    def update_item_matrix(self, data, user_matrix, item_matrix, num_features, lambda_item):
-        """
-        Method for update the item matrix
-
-        Parameters
-        ----------
-        data : sparse.lil.lil_matrix
-            data matrix.
-        user_matrix : np.ndarray
-            User matrix.
-        item_matrix : np.ndarray
-            Item matrix.
-        num_features : int
-            Number of features.
-        lambda_item : float
-            Regularization parameter for the item matirx.
-
-        Returns
-        -------
-        item_matrix : np.ndarray
-            Updated item matrix.
-
-        """
-        lambda_I = lambda_item * sp.eye(num_features)
-        for i in range(self.num_items):
-            indices = self.get_observed_users_per_item(data, i)
-            U = user_matrix[indices, :]
-            A = U.T @ U + lambda_I
-            B = U.T @ data[indices, i]
-            item_matrix[:, i] = np.linalg.solve(A, B).squeeze()
-        return item_matrix
 
     def get_train_test_matrix(self, train_indices, test_indices):
         """
@@ -182,59 +95,21 @@ class ALS(BaseEstimator, TransformerMixin):
         test[test_nnz_users, test_nnz_items] = self.data_matrix[test_nnz_users, test_nnz_items]
         return train, test
 
-    def get_observed_items_per_user(self, data, user_idx):
+    def fit(self, data, num_features, lambda_user, lambda_item, gamma):
         """
-        This method returns a list of all nonzero indices for a given user in the
-        data matirx
-
-        Parameters
-        ----------
-        data : sparse.lil.lil_matrix
-            Data matrix.
-        user_idx : int
-            ID of the user.
-
-        Returns
-        -------
-        np.ndarray
-            List of all nonzero indices for the given user.
-
-        """
-        return data.getrow(user_idx).nonzero()[1]
-
-    def get_observed_users_per_item(self, data, item_idx):
-        """
-        This method returns a list of all nonzero indices for a given item in the
-        data matrix
-
-        Parameters
-        ----------
-        data : sparse.lil.lil_matrix
-            Data matrix.
-        item_idx : int
-            ID of the item.
-
-        Returns
-        -------
-        np.ndarray
-            List of all nonzero indices for a given item.
-
-        """
-        return data.getcol(item_idx).nonzero()[0]
-
-    def fit(self, data, num_features, lambda_user, lambda_item):
-        """
-        Function that fits a user and item matrix
+        Function that fits the user and the item matrix.
 
         Parameters
         ----------
         data : sparse.lil.lil_matrix
             Data matrix.
         num_features : int
-            Number of latent features of the data.
+            Number of features.
         lambda_user : float
             Regularization parameter.
         lambda_item : float
+            Regularization parameter.
+        gamma : float
             Regularization parameter.
 
         Returns
@@ -242,17 +117,24 @@ class ALS(BaseEstimator, TransformerMixin):
         None.
 
         """
-
         user_matrix, item_matrix = self.init_matrix(data, num_features)
+        nnz_users, nnz_items = data.nonzero()
+        nnz_data = list(zip(nnz_users, nnz_items))
         for it in tqdm(range(self.num_epochs)):
-            user_matrix = self.update_user_matrix(data, user_matrix, item_matrix, num_features, lambda_user)
-            item_matrix = self.update_item_matrix(data, user_matrix, item_matrix, num_features, lambda_item)
+            np.random.shuffle(nnz_data)
+            for u, i in nnz_data:
+                user = user_matrix[u, :]
+                item = item_matrix[:, i]
+                err = data[u, i] - user @ item
+                user_matrix[u, :] += gamma * (err * item - lambda_user * user)
+                item_matrix[:, i] += gamma * (err * user - lambda_item * item)
+
         self.user_matrix = user_matrix
         self.item_matrix = item_matrix
 
     def get_test_rmse(self, test):
         """
-        Return rmse for the test data
+        Return rmse of the test data
 
         Parameters
         ----------
@@ -265,8 +147,8 @@ class ALS(BaseEstimator, TransformerMixin):
             RMSE for the test data.
 
         """
-        nnz_row, nnz_col = test.nonzero()
-        nnz_test = list(zip(nnz_row, nnz_col))
+        nnz_user, nnz_item = test.nonzero()
+        nnz_test = list(zip(nnz_user, nnz_item))
         rmse = 0.0
         for u, i in nnz_test:
             user = self.user_matrix[u, :]
@@ -280,18 +162,20 @@ class ALS(BaseEstimator, TransformerMixin):
         rmse = np.sqrt(rmse / len(nnz_test))
         return rmse
 
-    def optimizer_function(self, num_features, lambda_user, lambda_item):
+    def optimizer_function(self, num_features, lambda_user, lambda_item, gamma):
         """
         This function is used for optimize the hyperparameters with the Gaussian process.
 
         Parameters
         ----------
         num_features : int
-            Number of latent features.
+            Number of features.
         lambda_user : float
-            Regularization parameter for the user.
+            Regularization parameter.
         lambda_item : float
-            Regularization parameter for the item.
+            Regularization parameter.
+        gamma : float
+            Regularization parameter.
 
         Returns
         -------
@@ -304,7 +188,7 @@ class ALS(BaseEstimator, TransformerMixin):
         test_RMSE_list = []
         for train_indices, test_indices in kf.split(self.indices):
             train, test = self.get_train_test_matrix(train_indices, test_indices)
-            self.fit(train, int(num_features), lambda_user, lambda_item)
+            self.fit(train, int(num_features), lambda_user, lambda_item, gamma)
             test_rmse = self.get_test_rmse(test)
             test_RMSE_list.append(test_rmse)
         mean_test_rmse = np.mean(test_RMSE_list)
@@ -312,7 +196,7 @@ class ALS(BaseEstimator, TransformerMixin):
 
     def log_hyperparams_to_json(self):
         """
-        This function is used for storing the optimal hyperparameters
+        This funciton is used for logging the optimal hyperparameters.
 
         Returns
         -------
@@ -322,7 +206,13 @@ class ALS(BaseEstimator, TransformerMixin):
         optimizer = BayesianOptimization(
             f=self.optimizer_function, pbounds=self.tuning_params, random_state=self.random_state
         )
-        path = get_git_root() + "/logs/ALS.json"
+        path = get_git_root() + "/logs/SGD.json"
         logger = JSONLogger(path=path)
         optimizer.subscribe(Events.OPTIMIZATION_STEP, logger)
-        optimizer.maximize(init_points=4, n_iter=10)
+        optimizer.maximize(init_points=7, n_iter=20)
+
+
+if __name__ == "__main__":
+
+    SGD = SGD()
+    SGD.log_hyperparams_to_json()
